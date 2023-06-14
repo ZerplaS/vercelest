c9fe4822a032ff1ea9a1e8720927a6da6f7ee7b3
ZerplaS <86114184+ZerplaS@users.noreply.github.com>
2023-06-14 08:48:42 +0700

    sub

--- pages/shop.js
+++ pages/shop.js
@@ -109,7 +109,7 @@ export default function shop() {
       </div>
       <div className="pricing-header p-3 pb-md-4 mx-auto text-center">
         <h1 className="display-4 fw-normal text-body-emphasis">Pricing</h1>
-        <p className="fs-5 text-body-secondary">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+        <p className="fs-5 text-body-secondary">Plan for subscribtions!.</p>
       </div>
     </header>
     <main>
